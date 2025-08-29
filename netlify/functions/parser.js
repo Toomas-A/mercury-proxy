0fabaa01cf6c3fab72e8a487b54e2a0d48c43663
Toomas-A <58360070+Toomas-A@users.noreply.github.com>
2025-08-29 10:48:45 +0300

    Update parser.js

--- netlify/functions/parser.js
+++ netlify/functions/parser.js
@@ -1,11 +1,8 @@
 const Mercury = require('@postlight/mercury-parser');
 
 exports.handler = async (event) => {
-  // Получаем URL из параметров запроса.
-  // Например, из запроса вида: your-function.netlify.app/.netlify/functions/parser?url=https://your-website.com
   const articleUrl = event.queryStringParameters.url;
 
-  // Проверяем, был ли передан URL.
   if (!articleUrl) {
     return {
       statusCode: 400,
@@ -14,15 +11,19 @@ exports.handler = async (event) => {
   }
 
   try {
-    // Используем Mercury Parser для извлечения основного контента статьи.
     const result = await Mercury.parse(articleUrl);
-    
-    // Возвращаем очищенный текст в качестве ответа.
-    // Mercury также предоставляет поле `content` с HTML, но нам нужен только `text`.
-    return {
-      statusCode: 200,
-      body: result.content,
-    };
+
+    if (result && result.text) {
+      return {
+        statusCode: 200,
+        body: result.text,
+      };
+    } else {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ error: 'Could not extract text from the URL' }),
+      };
+    }
   } catch (error) {
     console.error('Parsing failed:', error);
     return {
